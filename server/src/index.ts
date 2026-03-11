403f8847a712556ac9e79eb472334b5743c00e91
xiaoweizhao <zhaoxiaoweiiiii@gmail.com>
2026-03-11 17:52:03 +0800

    Added another .gitignore file

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,7 +26,7 @@ app.use("/products", productRoutes);
 app.use("/users", userRoutes);
 app.use("/expenses", expenseRoutes);
 /* SERVER SETUP */
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = Number(process.env.PORT) || 5000;
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
